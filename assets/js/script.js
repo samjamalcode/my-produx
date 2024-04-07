303309b119f23aede470362a0dd482a04a9a97a5
samjamalcode <samjamalcode@gmail.com>
2024-04-07 08:14:49 +0000

    Initialize DOM elements, create getTotal function and add events to html elements

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// Initialize DOM elements
+let title = document.getElementById("title");
+let price = document.getElementById("price");
+let taxes = document.getElementById("taxes");
+let ads = document.getElementById("ads");
+let discount = document.getElementById("discount");
+let total = document.getElementById("total");
+let count = document.getElementById("count");
+let category = document.getElementById("category");
+let submit = document.getElementById("submit");
+
+// Function for calculating total
+function getTotal() {
+    if (price.value != 0) {
+      let result = +price.value + +taxes.value + +ads.value - +discount.value;
+      total.innerHTML = result;
+      total.style.backgroundColor = "green";
+    } else {
+      total.innerHTML = "";
+      total.style.backgroundColor = "#db1d0f";
+    }
+  }
